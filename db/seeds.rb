c97081b2769fe4db3a1247eb294efa712fb56c1e
kevinneyer <kevinneyer@gmail.com>
2020-06-04 23:24:04 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+yo =Article.create(title: "yo", description: "ma")
